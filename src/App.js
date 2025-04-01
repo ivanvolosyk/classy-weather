1364020d37e49c587b47d5d3872e21f339a77fcb
Ivan Volosyk <volosyk.vanya@gmail.com>
2025-04-01 20:40:42 +0300

    Add a local storage

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends React.Component {
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return;
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -65,24 +66,34 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", this.state.location);
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search from location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
-        <button onClick={this.fetchWeather}>Get weather</button>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
 
         {this.state.isLoading && <p className="loader">Loading</p>}
 
@@ -98,6 +109,19 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <input
+        type="text"
+        placeholder="Search from location..."
+        value={this.props.location}
+        onChange={this.props.onChangeLocation}
+      />
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
